9e163c7ee338bcc4413262593d43b7cf51fbd13c
Luisfernandolopez9 <ll1010745@est.univalle.edu>
2023-05-10 14:21:55 -0400

    git 2

--- Program.cs
+++ Program.cs
@@ -8,6 +8,9 @@
             int suma1, suma2 ,resultado;
             suma1= 1;
             suma2= 2;
+            string programacion = "github";
+           
+
             resultado= suma1 +suma2;
             Console.WriteLine("el resultado de la suma es " +resultado);
         }
