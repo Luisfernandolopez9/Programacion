7caf838cf9553fc48f043b51205658e4409f2131
Luisfernandolopez9 <ll1010745@est.univalle.edu>
2023-05-10 14:16:22 -0400

    mi primer hola mundo

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Programacion_Clase_Git
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo");
+            int suma1, suma2 ,resultado;
+            suma1= 1;
+            suma2= 2;
+            resultado= suma1 +suma2;
+            Console.WriteLine("el resultado de la suma es " +resultado);
+        }
+    }
+}
